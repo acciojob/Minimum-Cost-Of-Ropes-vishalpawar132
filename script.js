c2e903c15f2bbe24fafa492e29f8193fdfd217fa
vishal Pawar <133097620+vishalpawar132@users.noreply.github.com>
2023-07-24 09:56:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function compare(a, b) {
+  if (a < b) {
+      return -1;
+  } else if (a > b) {
+      return 1;
+  } else {
+      return 0;
+  }
+}
+ 
+ 
  
 function calculateMinCost() {
   //your code here
